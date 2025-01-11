299ec5b6a2c05bdb0cabddd7562300e776fdb636
Railey Pacheco <111040098+Railey29@users.noreply.github.com>
2025-01-10 16:28:29 -0800

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
-// tailwind.config.js
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{html,js}", // Ensure public folder is included
   ],
   theme: {
     extend: {},
